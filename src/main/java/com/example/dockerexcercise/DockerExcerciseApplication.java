14b72e41f93aeb9a51da888748e75d9718d81408
김승은 <tmddms0317@naver.com>
2023-05-16 14:10:36 +0900

    first commit

---
+++ src/main/java/com/example/dockerexcercise/DockerExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerExcerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerexcercise/DockerExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
